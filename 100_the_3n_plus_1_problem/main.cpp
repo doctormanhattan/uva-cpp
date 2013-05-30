f2fdb01ada2f57e94ee625dfd9b70815f5460332
Jonathan Osterman <drmanhattan@gmx.us>
2013-05-31 00:19:14 +0530

    Solved 100

--- 100_the_3n_plus_1_problem/main.cpp
+++ 100_the_3n_plus_1_problem/main.cpp
@@ -1,4 +1,40 @@
 #include <iostream>
+#include <fstream>
+
+
+bool is_odd(int n) { 
+
+	return (n%2) != 0 ; 
+}
+
+int next(int n) {
+
+	if (is_odd(n))
+		return 3*n + 1;
+	else
+		return n/2;
+}
+
+int sequence_length(int n) {
+
+	if ( n == 1)
+		return 1;
+	else
+		return 1 + sequence_length(next(n));
+	
+}
+
+int max_sequence_length(int from, int to) {
+
+	int max = sequence_length(from);
+
+	for(int i = from; i <= to; i++) {
+		if(max < sequence_length(i))
+			max = sequence_length(i);
+	}
+
+	return max;
+}
 
 void usage(std::ostream &os, std::string progname) {
 	os << "USAGE: " << progname << " <input-file> " << std::endl;
@@ -10,6 +46,15 @@ int main(int argc, char *argv[]) {
 		usage(std::cerr, argv[0]);
 		return 1;
 	}
+	
+	std::ifstream ifs(argv[1]);
+
+	int start;
+	int end;
+
+	while( ifs >> start >> end ) 
+		std::cout << start << " " << end << " " << max_sequence_length(start, end) << std::endl;
+
 
 	return 0;
 }
